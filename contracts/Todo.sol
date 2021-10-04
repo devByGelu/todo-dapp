9a784c869fe5917bd4b1f01e93c666d783decf65
200610313@my.xu.edu.ph <200610313@my.xu.edu.ph>
2021-10-04 10:20:23 +0800

    refactor Todo

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -2,6 +2,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Todo {
-  constructor() public {
+  struct STodo {
+    bytes32 description;
+  }
+
+  mapping(address => STodo[]) public todos;
+
+  constructor() public {}
+
+  function addTodo(bytes32 description) public returns (uint256 addedIndex) {
+    todos[msg.sender].push(STodo(description));
+    return todos[msg.sender].length;
+  }
+
+  function remove(uint256 index) public returns (uint256 newLength) {
+    delete todos[msg.sender][index];
+    return todos[msg.sender].length;
+  }
+
+  function getTodos() public view returns (bytes32[] memory ret) {
+    STodo[] memory t;
+    t = todos[msg.sender];
+    bytes32[] memory data = new bytes32[](t.length);
+    for (uint256 index = 0; index < t.length; index++) {
+      data[index] = t[index].description;
+    }
+    ret = data;
   }
 }
